6b48c1ba1141d5df2a7b2e3d8d2f380bf4babffa
Cephra <soulzterx@googlemail.com>
2024-05-19 03:26:07 +0200

    cleanup the script

--- src/app.js
+++ src/app.js
@@ -3,20 +3,26 @@ const cursor = "|";
 document.querySelectorAll(".text>*:not(.line)").forEach((el) => {
   const originalText = el.textContent;
   const originalTextLength = originalText.length;
+  const timeout = 0x29a / originalTextLength;
 
   el.querySelectorAll("span").forEach((span) => (span.textContent = "\xA0"));
 
   setTimeout(() => {
     el.querySelectorAll("span").forEach((span, i) => {
       setTimeout(() => {
-        span.classList.add('glow');
+        span.textContent = cursor;
+        span.classList.add("glow");
 
         if (i > 0) {
-          el.childNodes[i-1].textContent = originalText.substring(i-1, i);
+          el.childNodes[i - 1].textContent = originalText.substring(i - 1, i);
         }
 
-        span.textContent = (i < originalTextLength-1) ? cursor : originalText.substring(i, i+1);
-      }, (0x29a/originalTextLength)*i)
+        if (i === originalTextLength - 1) {
+          setTimeout(() => {
+            span.textContent = originalText.substring(i, i + 1);
+          }, timeout);
+        }
+      }, timeout * i);
     });
   }, 0x29a);
 });
