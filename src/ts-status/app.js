dddeaad21419c16bc709f15c35d66d01fca909c6
Cephra <soulzterx@googlemail.com>
2024-05-21 00:40:06 +0200

    added live update

--- src/ts-status/app.js
+++ src/ts-status/app.js
@@ -2,26 +2,41 @@ import { hideAllTexts, typeElem } from "../util";
 
 import axios from "axios";
 
+async function updateClientCount() {
+  let clients = await axios.get("https://ts.0x29a.me/api/clientlist");
+  const filteredClients = clients.data.body.filter(
+    (client) => client.client_type === "0",
+  );
+  const el = document.querySelector(".text > h1");
+  let filteredClientsLength = filteredClients.length.toString();
+
+  if (el.textContent !== filteredClientsLength) {
+    el.innerHTML = "";
+    filteredClientsLength.split().forEach((c) => {
+      const clientNumberSpan = document.createElement("span");
+      clientNumberSpan.classList.add("fadetext");
+      clientNumberSpan.textContent = c;
+      el.appendChild(clientNumberSpan);
+    });
+  }
+
+  setTimeout(() => {
+    requestAnimationFrame(updateClientCount);
+  }, 0x29a * 0x3ff);
+}
+
+async function runner() {
+  try {
+    setTimeout(() => {
+      updateClientCount();
+    }, 0x29a);
+  } catch (err) {
+    console.log(err);
+    // todo retry
+  }
+}
+
 hideAllTexts();
 typeElem(document.querySelector(".text > p"));
 
-axios
-  .get("https://ts.0x29a.me/api/clientlist")
-  .then((res) => {
-    const filteredClients = res.data.body.filter(
-      (client) => client.client_type === "0",
-    );
-    setTimeout(() => {
-      const el = document.querySelector(".text > h1");
-      el.innerHTML = "";
-      `${filteredClients.length}`.split().forEach((c) => {
-        const clientNumberSpan = document.createElement("span");
-        clientNumberSpan.classList.add("fadetext");
-        clientNumberSpan.textContent = c;
-        el.appendChild(clientNumberSpan);
-      });
-    }, 0x29a);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+runner();
