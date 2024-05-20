c2b61058ae5117bc32b7c4d1d6b9b53db621b55d
Cephra <soulzterx@googlemail.com>
2024-05-20 22:07:31 +0200

    added timeout for fade in effect

--- src/ts-status/app.js
+++ src/ts-status/app.js
@@ -11,12 +11,14 @@ axios
     const filteredClients = res.data.body.filter(
       (client) => client.client_type === "0",
     );
-    `${filteredClients.length}`.split().forEach((c) => {
-      const clientNumberSpan = document.createElement("span");
-      clientNumberSpan.classList.add("fadetext");
-      clientNumberSpan.textContent = c;
-      document.querySelector(".text > h1").appendChild(clientNumberSpan);
-    });
+    setTimeout(() => {
+      `${filteredClients.length}`.split().forEach((c) => {
+        const clientNumberSpan = document.createElement("span");
+        clientNumberSpan.classList.add("fadetext");
+        clientNumberSpan.textContent = c;
+        document.querySelector(".text > h1").appendChild(clientNumberSpan);
+      });
+    }, 0x29a);
   })
   .catch((err) => {
     console.log(err);
