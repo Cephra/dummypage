34cee913f5623044fda844e18b74c61f820f9630
Cephra <soulzterx@googlemail.com>
2024-05-20 22:06:09 +0200

    added prettier, improved animation in ts status, applied prettier

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # Install dependencies
+
 ```
 npm install
 ```
 
 # Clean local
+
 ```
 npm run clean
 ```
 
 # Start in dev
+
 ```
 npm run start
 ```
 
 # Build for prod
+
 ```
 npm run build
 ```
--- src/ts-status/app.js
+++ src/ts-status/app.js
@@ -3,17 +3,20 @@ import { hideAllTexts, typeElem } from "../util";
 import axios from "axios";
 
 hideAllTexts();
-typeElem(document.querySelector('.text > p'));
+typeElem(document.querySelector(".text > p"));
 
 axios
   .get("https://ts.0x29a.me/api/clientlist")
   .then((res) => {
     const filteredClients = res.data.body.filter(
-      (client) => client.client_type === "0"
+      (client) => client.client_type === "0",
     );
-    document.querySelector(".text > h1").dataset.originalText =
-      filteredClients.length;
-    typeElem(document.querySelector('.text > h1'));
+    `${filteredClients.length}`.split().forEach((c) => {
+      const clientNumberSpan = document.createElement("span");
+      clientNumberSpan.classList.add("fadetext");
+      clientNumberSpan.textContent = c;
+      document.querySelector(".text > h1").appendChild(clientNumberSpan);
+    });
   })
   .catch((err) => {
     console.log(err);
