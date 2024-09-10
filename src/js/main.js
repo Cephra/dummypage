a52579581f4c83d59db826bbbffa994ee90ede30
Cephra <soulzterx@googlemail.com>
2024-09-10 23:21:10 +0200

    use sleep

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,19 @@
 import "./rippler";
 import { createTyper } from "./typer";
+import { sleep } from "./util";
 
 async function main() {
   const typers = Array.from(
     document.querySelectorAll(".text>*:not(.line)"),
   ).map((el) => createTyper(el));
   typers.forEach(async (typer) => await typer.hide());
-  setTimeout(async () => {
-    const typerTypers = typers.map((typer) => {
-      return typer.type;
-    });
-    for (const typer of typerTypers) {
-      await typer();
-    }
-  }, 0x29a);
+  await sleep(0x29a);
+  const typerTypers = typers.map((typer) => {
+    return typer.type;
+  });
+  for (const typer of typerTypers) {
+    await typer();
+  }
+  await sleep(0x29a);
 }
 document.addEventListener("DOMContentLoaded", main);
