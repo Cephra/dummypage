fd6d10e74d33796fb7b2aebd66511488ef3a04d8
Cephra <soulzterx@googlemail.com>
2024-10-27 02:16:29 +0100

    feat: share button test

--- src/js/ts-status.js
+++ src/js/ts-status.js
@@ -45,7 +45,15 @@ async function runner() {
   } catch (err) {
     retry();
   }
-
+  
+  document.querySelector('#shareButton').addEventListener('click', async () => {
+    await navigator.share({
+      title: "TeamSpeak status",
+      text: `There are currently ${clientCount} clients in teamspeak.`,
+      url: "https://ts.0x29a.me/",
+    });
+  });
+  
   const typers = [
     createTyper(document.querySelector(".subcontainer > p")),
     createTyper(document.querySelector(".hint")),
