9b6ce05fd502b142c7b861a63876ad521683c5e4
Cephra <soulzterx@googlemail.com>
2024-12-07 17:57:10 +0100

    fix: restore current animation

--- src/js/main.js
+++ src/js/main.js
@@ -7,14 +7,16 @@ async function main() {
     document.querySelectorAll(".textcontainer > *, .subcontainer > *"),
   ).map((el) => createTyper(el));
   typers.forEach(async (typer) => await typer.hide());
-  await sleep(0x29a);
   const typerTypers = typers.map((typer) => {
     return typer.type;
   });
+  typerTypers.shift()();
+  await sleep(0x29a*2);
   for (const typer of typerTypers) {
     await typer();
   }
-  await sleep(0x29a);
-  typers.forEach(async (typer) => await typer.untype());
+  //TODO implement random sentence generation for the last typer
+  //const lastTyper = typers.at(-1);
+  //await lastTyper.untype();
 }
 document.addEventListener("DOMContentLoaded", main);
