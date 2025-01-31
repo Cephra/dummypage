0cf61f9dc5730685d09cf3621f1d92b7fa27da21
Cephra <soulzterx@googlemail.com>
2025-01-31 21:33:06 +0100

    feat: player list modal v0.1

--- src/js/ts-status.js
+++ src/js/ts-status.js
@@ -8,9 +8,10 @@ const retryInterval = 0x29a * 0x69;
 
 let firstFetch = true;
 let clientCount = 0;
+let filteredClients = [];
 async function updateClientCount(noRetry = false) {
   let clients = await axios.get("https://ts.0x29a.me/api/clientlist");
-  const filteredClients = clients.data.body.filter(
+  filteredClients = clients.data.body.filter(
     (client) => client.client_type === "0",
   );
   const el = document.querySelector(".textcontainer > h1");
@@ -55,6 +56,35 @@ async function runner() {
     const clipboardText = `There are currently ${clientCount} clients in teamspeak.`;
     navigator.clipboard.writeText(clipboardText);
   });
+  
+  const listButton = document.querySelector('#listButton');
+  const modalBackdrop = document.querySelector('#modalBackdrop');
+  const modal = document.querySelector('#modalBackdrop .modal');
+  const playerListUl = document.querySelector('#modalBackdrop .modal-body > ul');
+  listButton?.addEventListener('click', async (e) => {
+    e.stopPropagation();
+    
+    playerListUl.replaceChildren(
+      ...filteredClients.map((client) => {
+        const clientElem = document.createElement("li");
+
+        clientElem.textContent = client.client_nickname;
+
+        return clientElem;
+      }),
+    );
+    
+    modalBackdrop.classList.toggle('active',
+      !modalBackdrop.classList.contains('active')
+    );
+  });
+  modal.addEventListener('click', async (e) => {
+    e.stopPropagation();
+  });
+  document.addEventListener('click', async (e) => {
+    modal.contains(e.target) || modalBackdrop.classList.remove('active');
+  });
+  
   
   const typers = [
     createTyper(document.querySelector(".subcontainer > p")),
