ce03ca6faec3e2c163c7d58de58886c7f9eae489
Cephra <soulzterx@googlemail.com>
2025-07-05 22:52:15 +0200

    refactor: typer logic

--- src/js/typer.ts
+++ src/js/typer.ts
@@ -1,30 +1,33 @@
 const CURSOR = "|";
-const NBSP = "\xA0";
 
 import { sleep } from "./util";
 
 export const createTyper = (letterContainer: HTMLElement) => {
   const text = letterContainer.textContent?.trim() ?? "";
-  let splittedText = text.split("");
-  const timeout = 0x29a / splittedText.length;
-
+  let splittedText: Array<string>, timeout: number;
+  function updateSplittedTextAndTimeout(text: string) {
+    splittedText = text.split("");
+    timeout = 0x29a / splittedText.length;
+  }
+  updateSplittedTextAndTimeout(text)
+  
   const typer = {
     async changeText(newText: string, wait: number = 0) {
       if ("true" === letterContainer.dataset.hidden) {
-        splittedText = newText.split("");
         await typer.hide();
+        updateSplittedTextAndTimeout(newText)
       } else {
         await typer.untype();
-        splittedText = newText.split("");
         await typer.hide();
         await sleep(wait);
+        updateSplittedTextAndTimeout(newText)
         await typer.type();
       }
     },
     async hide() {
-      const letterElems = splittedText.map(() => {
+      const letterElems = splittedText.map((c) => {
         const letterElem = document.createElement("span");
-        letterElem.textContent = NBSP;
+        letterElem.textContent = c;
 
         return letterElem;
       });
@@ -42,40 +45,28 @@ export const createTyper = (letterContainer: HTMLElement) => {
       } else {
         letterElems = Array.from(letterContainer.querySelectorAll("span"));
       }
-      let prevElem: HTMLElement | null = null;
-      for (const [i, letterElem] of Array.from(letterElems.entries())) {
+      for (const letterElem of letterElems) {
+        let tmpLetter = letterElem.textContent;
         letterElem.textContent = CURSOR;
-        letterElem.classList.add("glow");
-
-        if (prevElem) {
-          prevElem.textContent = splittedText[i - 1];
-        }
-
-        prevElem = letterElem;
+        letterElem.classList.add("glow", "show");
 
         await sleep(timeout);
+        letterElem.textContent = tmpLetter;
       }
-      prevElem.textContent = splittedText.at(-1);
       letterContainer.dataset.hidden = "false";
     },
     async untype() {
       const letterElems = Array.from(letterContainer.querySelectorAll("span"));
 
-      let prevElem = null;
-
       for (const letterElem of letterElems.toReversed()) {
+        let tmpLetter = letterElem.textContent;
         letterElem.textContent = CURSOR;
 
-        if (prevElem) {
-          prevElem.textContent = NBSP;
-        }
-
-        prevElem = letterElem;
-
         await sleep(timeout);
+        letterElem.classList.remove("glow", "show");
+        letterElem.textContent = tmpLetter
       }
 
-      prevElem.textContent = NBSP;
       letterContainer.dataset.hidden = "true";
     },
   };
